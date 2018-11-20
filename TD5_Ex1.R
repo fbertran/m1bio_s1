074af2d345bc2b3c1c0fe9dfc018b9935475c12f
fbertran <fbertran@unistra.fr>
2018-11-20 10:47:06 +0100

    Update TD5_Ex1.R

--- TD5_Ex1.R
+++ TD5_Ex1.R
@@ -1,8 +1,14 @@
 #Exercice 1
 setwd("myDir")
 Cyclamen<-read.csv("TD5_Ex1.CSV",colClasses=c("factor","numeric","factor"))
-replications(Vitesse~Plante%in%Milieu, data=Cyclamen)
+
+library(lattice)
+xyplot(Vitesse~Milieu|Plante,data=Cyclamen,pch=19)
+
+replications(Vitesse~Milieu + Plante%in%Milieu, data=Cyclamen)
 Modele1 <- aov(Vitesse~Plante%in%Milieu,data=Cyclamen)
 shapiro.test(residuals(Modele1))
 bartlett.test(residuals(Modele1),I((Cyclamen$Plante):(Cyclamen$Milieu)))
 summary(Modele1)
+
+xyplot(residuals(Modele1)~Milieu|Plante,data=Cyclamen,pch=19)
