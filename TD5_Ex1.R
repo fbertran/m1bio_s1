d99a67230514d2910c6f94f31f0298ba5d5cfc15
fbertran <fbertran@unistra.fr>
2018-11-20 10:33:24 +0100

    Create TD5_Ex1.R

---
+++ TD5_Ex1.R
@@ -0,0 +1,8 @@
+#Exercice 17
+setwd("myDir")
+Cyclamen<-read.csv("TD5_Ex1.CSV",colClasses=c("factor","numeric","factor"))
+replications(Vitesse~Plante%in%Milieu, data=Cyclamen)
+Modele1 <- aov(Vitesse~Plante%in%Milieu,data=Cyclamen)
+shapiro.test(residuals(Modele1))
+bartlett.test(residuals(Modele1),I((Cyclamen$Plante):(Cyclamen$Milieu)))
+summary(Modele1)
