7b375c1f5236b741f1ecaef39b052581b5e89c4a
fbertran <fbertran@unistra.fr>
2018-12-03 11:27:12 +0100

    Update TD_NP.R

--- TD_NP.R
+++ TD_NP.R
@@ -5,15 +5,35 @@ install.packages("BSDA")
 library(BSDA)
 SIGN.test(poids,md = .65)
 
-wilcox.test(poids)
-#library(coin)
-#wilcoxsign_test(poids)
-
 #Symétrie ?
 #Pas par rapport à 0 !
 #Mais par rapport à -.04 pourquoi pas.
 hist(poids-.65)
 plot(density(poids-.65))
 
+wilcox.test(poids, mu = .65)
+#library(coin)
+#wilcoxsign_test(poids)
+
+
+enfants = data.frame(matrix(c(82, 63,
+69, 42,
+73, 74,
+43, 37,
+58, 51,
+56, 43,
+76, 80,
+65, 62),byrow=TRUE,ncol=2))
+colnames(enfants) <- c("Scores enfants scolarisés","Scores enfants non-scolarisés")
+enfants
+wilcox.test(enfants[,1],enfants[,2],paired=TRUE)
+
+library(coin)
+enfants_long = data.frame(score=c(enfants[,1],enfants[,2]),group=c(rep("s",8),rep("ns",8)),jumeaux=factor(rep(1:8,2)))
+enfants_long
+#Ne pas utiliser, car repose sur une approximation et donc moins précis que le test exact.
+with(enfants_long,wilcoxsign_test(score~group|jumeaux, distribution = "asymptotic"))
+#Nous vérifions que la valeur calculée par défaut dans ce cas par la fonction wilcox.test est la valeur "exacte".
+with(enfants_long,wilcoxsign_test(score~group|jumeaux, distribution = "exact"))
 
 
