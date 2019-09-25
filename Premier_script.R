9e0894fd95aa4d6a85d5df2e1d5c89b04eecdd61
Frederic Bertrand <fbertran@math.unistra.fr>
2019-09-25 23:15:31 +0200

    Add files via upload

---
+++ Premier_script.R
@@ -0,0 +1,51 @@
+(9^2+19^2/22)^(1/4)
+99^2/2206/sqrt(2)
+#Ce ceux deux approxi-
+#mations de pi
+
+age = c(26, 25, 23, 19, 22, 21, 26, 
+27, 22, 18, 31, 32, 38, 27, 24, 25)
+
+age
+
+age^2
+
+sqrt(age)
+
+log(age)
+
+age[3]
+
+age[10:12]
+
+age[c(2,4,6)]
+
+age[-(1:5)]
+
+jeune=age[age<20]
+jeune
+
+length(age)
+sort(age)
+
+min(age)
+max(age)
+
+sum(age)
+
+mean(age)
+sd(age)
+
+median(age)
+#Le vrai mad
+mad(age, constant = 1)
+
+
+
+
+
+
+
+
+
+
